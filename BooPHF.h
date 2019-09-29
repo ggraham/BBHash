56e695543c59eb426ebb7c9032bff5e2f05cd89c
Mikko Rautiainen <m_rautiainen@hotmail.com>
2019-09-29 21:41:08 +0200

    stick everything to ramdisk

--- BooPHF.h
+++ BooPHF.h
@@ -1358,13 +1358,13 @@ we need this 2-functors scheme because HashFunctors won't work with unordered_ma
 			//printf("---process level %i   wr %i fast %i ---\n",i,_writeEachLevel,_fastmode);
 			
 			char fname_old[1000];
-			sprintf(fname_old,"temp_p%i_level_%i",_pid,i-2);
+			sprintf(fname_old,"/dev/shm/temp_p%i_level_%i",_pid,i-2);
 			
 			char fname_curr[1000];
-			sprintf(fname_curr,"temp_p%i_level_%i",_pid,i);
+			sprintf(fname_curr,"/dev/shm/temp_p%i_level_%i",_pid,i);
 			
 			char fname_prev[1000];
-			sprintf(fname_prev,"temp_p%i_level_%i",_pid,i-1);
+			sprintf(fname_prev,"/dev/shm/temp_p%i_level_%i",_pid,i-1);
 			
 			if(_writeEachLevel)
 			{
